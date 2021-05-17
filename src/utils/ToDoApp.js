556b646aa0fc7090821b928855b6284a940b510c
taylor-tokareff <taylortokareff@gmail.com>
2021-05-17 14:48:23 -0700

    basic structure work

---
+++ src/utils/ToDoApp.js
@@ -0,0 +1,53 @@
+import request from 'superagent';
+
+export async function signUp(credentials) {
+  const response = await request
+    .post('/api/auth/signup')
+    .ok(res => res.status < 500)
+    .send(credentials);
+
+  if (response.status === 400) {
+    throw response.body;
+  }
+
+  return response.body;
+}
+
+export async function signIn(credentials) {
+  const response = await request
+    .post('/api/auth/signin')
+    .ok(res => res.status < 500)
+    .send(credentials);
+
+  if (response.status === 400) {
+    throw response.body;
+  }
+
+  return response.body;
+}
+
+export async function addToDo(todo) {
+  const response = await request
+    .post('/api/todo')
+    .set('Authorization', window.localStorage.getItem('TOKEN'))
+    .send(todo);
+
+  return response.body;
+}
+
+export async function getToDos() {
+  const response = await request
+    .get('/api/me/todo')
+    .set('Authorization', window.localStorage.getItem('TOKEN'));
+
+  return response.body;
+}
+
+export async function deleteToDo(id) {
+  const response = await request
+    .delete(`/api/todo/${id}`)
+    .set('Authorization', window.localStorage.getItem('TOKEN'));
+
+  return response.body;
+}
+
